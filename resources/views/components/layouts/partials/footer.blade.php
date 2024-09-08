fe76dc3d19d6db705f2f0cc5142ec3eaae39269d
KevRodas <kevinrodas166@gmail.com>
2024-09-08 09:15:18 -0600

    proyecto final

---
+++ resources/views/components/layouts/partials/footer.blade.php
@@ -0,0 +1,7 @@
+<footer class="main-footer">
+    <strong>Proyecto Tesis &copy; 2024 <a href="#">Kevin Rodas</a></strong>
+    Sistema de ventas
+    <div class="float-right d-none d-sm-inline-block">
+      <b>Version</b> 1.2.0
+    </div>
+  </footer>
